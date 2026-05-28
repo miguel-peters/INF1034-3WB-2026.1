0185dfb4b170af1afcbd07ca22449b5df02a8c22
miguel-peters <miguelppeters@gmail.com>
2026-05-27 23:09:15 -0300

    Atividade 11 concluída

--- Atividades/INF1034 - 3WB - 2026.1 - Atividade 11.py
+++ Atividades/INF1034 - 3WB - 2026.1 - Atividade 11.py
@@ -1,63 +1,141 @@
-import pygame, sys
-from pygame.locals import QUIT, KEYDOWN
+from pygame import *
+import sys
 
-clock = pygame.time.Clock()
+clock = time.Clock()
 
-pygame.init()
-screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption('Hello World')
+estado = 'idle'
 
 
-#herro_walk_list = [] - começar vazia e ir inserindo as imagens dentro da lista
-# for i in range(4):
-    # herro_walk_list.append(pygame.image.load(f'Hero_Walk_0{i+1}'))
 
-#criar variável
-#current_frame = 0 - Representa o frame da animação que está sendo desenhado no momento
-# run_animation = False
+# ANIMAÇÃO 1
+curr_frame_idle = 0
+anim_time_idle = 0
+knight_idle = image.load('Imagens/IDLE.png')
 
-#prox animação
-#current_fram_mm = 0 - controla os frames cada pedaço da imagem
-#anim_time_mm = 0
-#imagem = pygame.image.load(imagem) - Como é uma spritesheet nao precisa de lista(assets/assets/imagem)
+# ANIMAÇÃO 2
+curr_frame_run = 0
+anim_time_run = 0
+knigth_run = image.load('Imagens/RUN.png')
+pos_x = 200
+
+curr_frame_runLFT = 0
+anim_time_runLFT = 0
+knigth_runLFT = image.load('Imagens/RUNLFT.png')
+
+# ANIMAÇÃO 3
+curr_frame_jump = 0
+anim_time_jump = 0
+knigth_jump = image.load('Imagens/JUMP.png')
+
+init()
+
+mixer.music.load('pokemon.mp3')
+mixer.music.play(-1)
+screen = display.set_mode((800, 600))
+display.set_caption('Hello World!')
 
 while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.qui()
+    for ev in event.get():
+        if ev.type == QUIT:
+            quit()
             sys.exit()
-        # if event.ype == KEYDOWN:
-            #if event.key == pygame.K_SPACE:
-                #run animation = TRUE
+        if ev.type == KEYDOWN:
+            if ev.key == K_SPACE:
+                estado = 'jump'
+
     clock.tick(60)
     dt = clock.get_time()
+    keys = key.get_pressed()
 
-    #anim_time = anime_time + dt
-    #anime_time_sec = anim_time/1000
+    if keys[K_d]:
+        estado = 'run'
+        pos_x = pos_x + 0.1*dt
+    elif not keys[K_d] and estado == 'run':
+        estado = 'idle'
+    if keys[K_a]:
+        estado = 'runLFT'
+        pos_x = pos_x - 0.1*dt
+    elif not keys[K_a] and estado == 'runLFT':
+        estado = 'idle'
 
-    #if anim_time_sec > 0.2: 
-        #current_frame += 1
-        # if current_frame > len(herro_walk_list) - 1:
-            #current_frame = 0
-        #anim_time_sec = 0
+    if estado == 'idle':
+        anim_time_idle = anim_time_idle + dt
+        anim_time_idle_sec = anim_time_idle/1000
 
-    # REPETE A ESTRUTURA PARA A SEGUNDA ANIMAÇÃO
-
-
-    screen.fill((255,255,255))
-
-    #screen.blit(variável da imagem, (coordenadas)) - Forma errada pois tem vários frames
-    #screen.blit(herro_walk_lis[current_frame], (coordenadas))
-
-    #se for um spreadsheet:
-    # if current fram < 5:
-        #screen.blit(imagem, (coordenadas), (area - 60 * current_frame,0,60,60- x,y, altura e largura)))
-    # else:
-        #mexe na coordenada y e current_frame - 5
-
-    # OU faz que nem a foto que eu tirei
+        if anim_time_idle > 100:
+            curr_frame_idle = curr_frame_idle + 1
+            if curr_frame_idle > 6:
+                curr_frame_idle = 0
+            anim_time_idle = 0
     
-    # if run_animation == True:
-        #FAZ O CÓDIGO DA ANIMAÇÂO usando um run_animation = False
+    if estado == 'run':
+        anim_time_run = anim_time_run + dt
+        anim_time_run_sec = anim_time_run/1000
+        
+        if anim_time_run_sec > 0.1:
+            curr_frame_run = curr_frame_run + 1
+            if curr_frame_run > 7:
+                curr_frame_run = 0
+            anim_time_run = 0
 
-    pygame.display.update()
+    if estado == 'runLFT':
+        anim_time_runLFT = anim_time_runLFT + dt
+        anim_time_run_secLFT = anim_time_runLFT/1000
+        
+        if anim_time_run_secLFT > 0.1:
+            curr_frame_runLFT = curr_frame_runLFT + 1
+            if curr_frame_runLFT > 7:
+                curr_frame_runLFT = 0
+            anim_time_runLFT = 0
+
+    if estado == 'jump':
+        anim_time_jump =  anim_time_jump + dt
+        anim_time_jump_sec = anim_time_jump/1000
+
+        if anim_time_jump_sec > 0.1:
+            curr_frame_jump = curr_frame_jump + 1
+            if curr_frame_jump > 4:
+                curr_frame_jump = 0
+                estado = 'idle'
+            anim_time_jump = 0
+
+    # Desenho dos elementos na tela
+    screen.fill((50, 4, 179))
+
+    # screen.blit(dog_img, (0, 0))
+    # screen.blit(hero_walk_list[curr_frame], (0, 0))
+
+    # if curr_frame_mm < 5:
+    #     screen.blit(megaman_spritesheet, (200, 200), (60 * curr_frame_mm, 0, 60, 60))
+    # else:
+    #     screen.blit(megaman_spritesheet, (200, 200), (60 * (curr_frame_mm - 5), 60, 60, 60))
+    
+    draw.rect(screen, (72, 157, 37), (0, 400, 1280, 200))
+
+    draw.rect(screen, (121,77,27), (500,300,40,140))
+    draw.circle(screen, (72, 157, 37), (520,270), 75)
+
+    draw.rect(screen, (121,77,27), (100,300,40,140))
+    draw.circle(screen, (72, 157, 37), (120,270), 75)
+
+    draw.rect(screen, (121,77,27), (250,270,40,140))
+    draw.circle(screen, (72, 157, 37), (270,240), 75)
+
+    if estado == 'run':
+        knigth_scale_run = transform.scale(knigth_run, (1200,150))
+        screen.blit(knigth_scale_run, (pos_x, 400), (150*(curr_frame_run), 0, 150, 150))
+
+    if estado == 'runLFT':
+        knight_scale_runLFT = transform.scale(knigth_runLFT, (1200, 150))
+        screen.blit(knight_scale_runLFT, (pos_x, 400), (150*(curr_frame_runLFT), 0, 150, 150))
+
+    if estado == 'idle':
+        knight_scale_idle = transform.scale(knight_idle, (1200,150))
+        screen.blit(knight_scale_idle, (pos_x,400), (171.5*(curr_frame_idle), 0, 171.5, 171.5))
+
+    if estado == 'jump':
+        knigth_scale_jump = transform.scale(knigth_jump, (1000,150))
+        screen.blit(knigth_scale_jump, (pos_x, 400), (200*(curr_frame_jump), 0, 200, 200))
+
+
+    display.update()
--- mai2n.py
+++ mai2n.py
@@ -1,33 +1,67 @@
-import pygame
-import random
+import pygame, sys
+from pygame.locals import QUIT, KEYDOWN
 
-# Inicializa o Pygame
+clock = pygame.time.Clock()
+
+# dog_img = pygame.image.load("chiens-funnyanimals.gif")
+
+# hero_img = pygame.image.load("assets/assets/Hero_Walk_01.png")
+
+# hero_walk_list = [
+#     pygame.image.load("assets/assets/Hero_Walk_01.png"),
+#     pygame.image.load("assets/assets/Hero_Walk_02.png"),
+#     pygame.image.load("assets/assets/Hero_Walk_03.png"),
+#     pygame.image.load("assets/assets/Hero_Walk_04.png")
+# ]
+
+# curr_frame = 0
+# anim_time = 0
+# hero_walk_list = []
+# for i in range(4):
+#     hero_walk_list.append(pygame.image.load(f"assets/assets/Hero_Walk_0{i+1}.png"))
+
+
+curr_frame_mm = 0
+anim_time_mm = 0
+megaman_spritesheet = pygame.image.load("RUN.png")
+
 pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Hello World!')
 
-# Cores iniciais
-tela = pygame.display.set_mode((400, 300))
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                run_animation = True
 
-# Função para gerar uma cor aleatória
-def cor_aleatoria():
-    vermelho = random.randint(0, 255)
-    verde = random.randint(0, 255)
-    azul = random.randint(0, 255)
-    return (vermelho, verde, azul)
+    clock.tick(60)
+    dt = clock.get_time()
+    
+    anim_time_mm = anim_time_mm + dt
+    anim_time_mm_sec = anim_time_mm/1000
 
-# Game loop
-rodando = True
-cor_atual = cor_aleatoria()
+    if anim_time_mm_sec > 0.1:
+        curr_frame_mm = curr_frame_mm + 1
+        # curr_frame_mm += 1
+        if curr_frame_mm > 7:
+            curr_frame_mm = 0
+        anim_time_mm = 0
 
-while rodando:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            rodando = False
-        # Clica na tela para mudar a cor para uma nova cor aleatória
-        elif evento.type == pygame.MOUSEBUTTONDOWN:
-            cor_atual = cor_aleatoria()
+    # Desenho dos elementos na tela
+    screen.fill((255,255,255))
 
-    # Preenche a tela com a cor aleatória gerada
-    tela.fill(cor_atual)
-    pygame.display.flip()
+    # screen.blit(dog_img, (0, 0))
+    # screen.blit(hero_walk_list[curr_frame], (0, 0))
 
-pygame.quit()
+    # if curr_frame_mm < 5:
+    #     screen.blit(megaman_spritesheet, (200, 200), (60 * curr_frame_mm, 0, 60, 60))
+    # else:
+    #     screen.blit(megaman_spritesheet, (200, 200), (60 * (curr_frame_mm - 5), 60, 60, 60))
+    
+    screen.blit(megaman_spritesheet, (200, 200), (96*(curr_frame_mm), 0, 96, 84))
+
+    pygame.display.update()
